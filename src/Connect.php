e1657e22812d7f17758f9814f0a33a778442ea50
QuestBlue <apps@questblue.com>
2023-01-06 17:48:38 -0500

    modify connect class to use curl wrapper

--- src/Connect.php
+++ src/Connect.php
@@ -2,6 +2,8 @@
 
 namespace questbluesdk;
 
+use questbluesdk\CurlWrapper\CurlRequest;
+
 class Connect
 {
     private $login;
@@ -17,41 +19,32 @@ class Connect
         $this->key = 'ACCOUNT_PRIVATE_KEY'; // Your API private key
         $this->type = 'json'; // API type, available values: json, xml
     }
-    
 
     function query($request, $params = [], $method = 'GET') 
     {
-        $ch = curl_init();
+        $headers = [
+            'Content-Type: application/json',
+            "Security-Key: {$this->key}"
+        ];
 
-        if($this->type == 'xml') {
-           curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/xml', 'Security-Key:' . $this->key) );  
-        } else {
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Security-Key:' . $this->key) );
-            if(is_array($params) && count($params) > 0) {
-                $params = json_encode($params);
-            } 
-        }
-        
-        curl_setopt($ch, CURLOPT_URL, 'https://api2.questblue.com/' . $request ); // Production
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-        curl_setopt($ch, CURLOPT_HEADER, 0); // Set 1 to debug
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'QuestBlue API v.2');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 45);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 45);
-        curl_setopt($ch, CURLOPT_USERPWD,  $this->login . ':' . $this->password);
+        if(is_array($params) && count($params) > 0) {
+            $params = json_encode($params);
+        } 
 
-        $response = curl_exec($ch);
-        $resp_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
+        $request = (new CurlRequest("https://api2.questblue.com/$request"))
+            ->setHeaders($headers)
+            ->setOption(CURLOPT_CUSTOMREQUEST, $method)
+            ->setOption(CURLOPT_HEADER, 0) // Set 1 to debug
+            ->setOption(CURLOPT_RETURNTRANSFER, 1)
+            ->setOption(CURLOPT_USERAGENT, 'QuestBlue API v.2')
+            ->setOption(CURLOPT_SSL_VERIFYPEER, 0)
+            ->setOption(CURLOPT_CONNECTTIMEOUT, 45)
+            ->setOption(CURLOPT_TIMEOUT, 45)
+            ->setOption(CURLOPT_POSTFIELDS, $params)
+            ->setOption(CURLOPT_USERPWD, "{$this->login}:{$this->password}");
 
-        // Debug
-        //echo '<br/>Server resp: <br><pre>'; var_dump($response); echo '</pre>response code: ' . $resp_code. '<br><br>';
+        $response = $request->execute();
 
         return $response === false ? 'API2 request error' : $response;
     }
-    
-
 }
