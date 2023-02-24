e8bbf6b67da208deddcdad9f28164e45412a49a0
QuestBlue <apps@questblue.com>
2023-02-24 15:54:19 -0500

    minor fix

--- src/Lnp.php
+++ src/Lnp.php
@@ -72,38 +72,7 @@ class Lnp extends Connect {
        
     public function updateLnp($params,$path = '')
     {
-        $data = [
-            'id' => 17740,                                   // Required
-            'foc_date'             => null, //'2020-07-22',        // Request Firm Order Commitment (FOC) date. Optional
-            'activate_time'        => null, //'14:00',             // Reuested FOC Activation time Eastern Standard Time (EST). Optional, from 00:00 to 23:00 Must be roud hour if set (12.00, 13.00 etc)
-            'trunk'                => null, //'myTrunkName',            // String. Optional. SIP trunk name to route the DID to. For voice DIDs only.
-            'partial_port'         => null, //'no',                // Values: no (default), yes
-            'extra_services'       => null, //'some extra333',     // String. Required if only partial_port == yes
-            'location'             => null, //'business',          // Values: business, residential. Required
-            'company'              => null, //'My Company Name3',  // Company name. Required if location is set to Business
-            'wireless_no'          => null, //'no',                // no(default), yes.
-            'pincode'              => null, //'6666',              // Wireless account PIN code. Required if  wireless_no == 1 only
-            'ssn'                  => null, //'7777',              // SSN last four digits. Required if  wireless_no == 1 only
-            'lidb_list'            => null, //'no',                // Values:   no (default), yes
-            'provider_name'        => null, //'AC&DC',             // Max. 255 chars. Required
-            'account_no'           => null, //'111',               // Max. 100 chars. Required
-            'authorize_contact'    => null, //'John Doe',          // Max. 15 chars. Required
-            'contact_title'        => null, //'CEO/Owner',         // Values: Employee, President, CEO/Owner.  Required
-            'street_no'            => null, //'17345',             // Max. 50 chars. Required
-
-             'dir_prefix'          => null, //'SE',               // Max. 10 chars. Values example: None, N, NE, E, SE, S, SW, W, NW. Optional
-            'street_name'          => null, //'Park Avenue',       // Max. 100 chars. Required
-            'dir_suffix'           => null, //'NE',                // Max. 10 chars. Values example: None, N, NE, E, SE, S, SW, W, NW. Optional
-            'service_unit'         => null, //'1666',              // Service Suit/Unit. Max. 100 chars. Optional
-            'city'                 => null, //'Durham',            // Service City. Max. 100 chars. Required
-            'state'                => null, //'AA',                 // Service State. Optional
-            'zipcode'              => null, //'7777',              // Service ZipCode Max. 20 chars. Required
-            'billing_telephone_no' => null, //'1234567890',        // Billing Telephone Number. Max. 20 chars.  Required
-            'port_out_pin'         => null,//'123',                    // Previous carrier port out PIN. Optional
-
-            // Phone bill, full path to GIF, JPG, PNG or PDF file. Size up to 2Mb
-            // 'testmode'      => 'error',
-        ];
+        $data = [];
 
         foreach($params as $key=>$value){
             $data[$key] = $value;
