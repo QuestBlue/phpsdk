a574d67f76294c271ecbb1dbea3caccb9b01c1eb
Joseph Williamson <jwilliamson@questblue.com>
2024-11-05 17:28:37 -0500

    dedicated class for Account Detail endpoint

---
+++ src/Models/AccountDetail.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace questbluesdk\Models;
+
+class AccountDetail
+{
+    public AccountDetailData $data;
+}
+
+class AccountDetailData
+{
+    public float $balance;
+    public float $minimumBalance;
+    public float $reloadAmount;
+    public float $allowedCredit;
+    public string $paymentMethod;
+    public int $lowBalanceAlertAmount;
+    public string $balanceAutorefill;
+    public string $balanceNotify;
+}
