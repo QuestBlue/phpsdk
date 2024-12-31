61b4a7e528778e87ad45f26ead070f8e985cee1a
Joseph Williamson <jwilliamson@questblue.com>
2024-12-31 12:18:20 -0500

    fix data types for create lnp response

--- src/Models/Responses/Lnp/CreateLnpResponse.php
+++ src/Models/Responses/Lnp/CreateLnpResponse.php
@@ -4,13 +4,14 @@ namespace questbluesdk\Models\Responses\Lnp;
 
 use JMS\Serializer\Annotation\Type;
 use questbluesdk\Models\Responses\BaseResponse;
+use questbluesdk\Models\Responses\Lnp\Data\CreateLnpData;
 
 class CreateLnpResponse extends BaseResponse
 {
-    #[Type(name: "array<questbluesdk\Models\Responses\Lnp\Data\CreateLnpData")]
-    protected array $data = [];
+    #[Type(name: "questbluesdk\Models\Responses\Lnp\Data\CreateLnpData")]
+    protected ?CreateLnpData $data = null;
 
-    public function getData(): array
+    public function getData(): ?CreateLnpData
     {
         return $this->data;
     }
